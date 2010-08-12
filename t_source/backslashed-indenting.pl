d43a8723838273d56f63751f6b5cc8c1e551da23
Andy Lester <andy@petdance.com>
2010-08-11 23:02:34 -0500

    more crazy

--- t_source/backslashed-indenting.pl
+++ t_source/backslashed-indenting.pl
@@ -7,7 +7,11 @@ my $bar;
 # There was a bug where indent/perl.vim saw a backslash as a closing
 # bracket, thus goofing up indents.
 if ( $foo ) {
-    if ( $bar ) {
+    if ( $bar +
+        $y +
+        $x
+        == 14
+    ) {
         $x = [
             qw(
             foo
