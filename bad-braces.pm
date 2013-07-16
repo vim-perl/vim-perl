455619a1dac3c2c4fb2559758e21afc68641aed3
Andy Lester <andy@petdance.com>
2013-07-16 09:17:53 -0500

    Examples of bad braces

---
+++ bad-braces.pm
@@ -0,0 +1,10 @@
+
+push( @ids, map { $_->{class}->id } @{$specs_flattened->{$set}} );
+
+my %hash = map { $_->{class}->id } @{$specs_flattened->{$set}} );
+
+my $hashref = { map { $_->{class}->id } @{$specs_flattened->{$set}} ) };
+
+sub foo {
+    push( @ids, map { $_->{class}->id } @{$specs_flattened->{$set}} );
+}
