1543f95703121ea65f053420b91d50e378a0ab92
Andy Lester <andy@petdance.com>
2013-07-16 09:22:22 -0500

    more notes

--- bad-braces.pm
+++ bad-braces.pm
@@ -1,14 +1,19 @@
 
+# These all look OK
+
 push( @ids, map { $_->{class}->id } @{$specs_flattened->{$set}} );
 
 my %hash = map { $_->{class}->id } @{$specs_flattened->{$set}} );
 
 my $hashref = { map { $_->{class}->id } @{$specs_flattened->{$set}} ) };
 
+for my $i ( 1..10 ) {
+    push( @ids, map { $_->{class}->id } @{$specs_flattened->{$set}} );
+}
+
+
+# For Andy, this is not colored right
 sub foo {
     push( @ids, map { $_->{class}->id } @{$specs_flattened->{$set}} );
 }
 
-for my $i ( 1..10 ) {
-    push( @ids, map { $_->{class}->id } @{$specs_flattened->{$set}} );
-}
